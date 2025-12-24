04c51acb133b70346adaf8da3a65592444730ce0
vishnubhotlasirichandana <sirichandanavishnubhotla@gmail.com>
2025-12-24 20:59:15 +0530

    Initial commit - Frontend UI

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import { AuthProvider } from './contexts/AuthContext'; // <--- Fixed Path
+import { AuthProvider } from './contexts/AuthContext';
+import { CartProvider } from './contexts/CartContext'; // <--- ADD THIS
 import './styles/global.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <App />
+        {/* CartProvider must be INSIDE AuthProvider so it can access the user */}
+        <CartProvider> 
+          <App />
+        </CartProvider>
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
